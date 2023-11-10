4fd7d55ab3e746dfe49a1d61e0807f7bebb4281e
AdharshPS <adharshps000@gmail.com>
2023-11-10 15:29:45 +0530

    colors

---
+++ lib/database/data_base.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class DataBase {
+  static List<Color> colorList = [
+    Colors.green,
+    Colors.blue,
+    Colors.red,
+    Colors.yellow,
+    Colors.purple,
+  ];
+}
--- lib/views/list_screen/list_screen.dart
+++ lib/views/list_screen/list_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_2/database/data_base.dart';
 import 'package:flutter_application_2/model/notes_model.dart';
 import 'package:flutter_application_2/views/list_screen/widgets/list_screen_widget.dart';
 
@@ -13,6 +14,8 @@ class _HomeScreenState extends State<HomeScreen> {
   final nameController = TextEditingController();
   final desController = TextEditingController();
   final dateController = TextEditingController();
+
+  String value = "";
 
   List<Color> colorList = [
     Colors.green,
@@ -47,151 +50,158 @@ class _HomeScreenState extends State<HomeScreen> {
               addList.removeAt(index);
               setState(() {});
             },
-            onEdit: () {},
+            onEdit: () {
+              value = "Update";
+              bottomSheet(context);
+              nameController.text = addList[index].title;
+              desController.text = addList[index].description;
+              dateController.text = addList[index].date;
+            },
           ),
         ),
       ),
-      floatingActionButton: FloatingActionButtonMethod(context),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {
+            value = "Submit";
+            bottomSheet(context);
+          }),
     );
   }
 
-  FloatingActionButton FloatingActionButtonMethod(BuildContext context) {
-    return FloatingActionButton(
-      onPressed: () {
-        showModalBottomSheet(
-          // isScrollControlled: true,
-          context: context,
-          builder: (context) {
-            return Column(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(height: 10),
-                // Expanded(
-                //   child: ListView.separated(
-                //     itemBuilder: (context, index) => Container(
-                //       width: 300,
-                //       decoration: BoxDecoration(
-                //         borderRadius: BorderRadius.circular(20),
-                //       ),
-                //       child: TextField(
-                //         decoration: InputDecoration(
-                //           contentPadding: EdgeInsets.symmetric(
-                //               vertical: 10, horizontal: 20),
-                //           filled: true,
-                //           fillColor: Colors.grey,
-                //           hintStyle: TextStyle(
-                //             fontSize: 18,
-                //             fontWeight: FontWeight.w900,
-                //           ),
-                //           hintText: bottomSheetContents[index],
-                //         ),
-                //       ),
-                //     ),
-                //     separatorBuilder: (context, index) => SizedBox(height: 10),
-                //     itemCount: 3,
-                //   ),
-                // ),
-                Container(
-                  width: 300,
-                  height: 40,
-                  padding: EdgeInsets.only(
-                    bottom: MediaQuery.of(context).viewInsets.bottom,
-                  ),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: TextFormField(
-                    controller: nameController,
-                    decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Colors.grey,
-                        hintStyle: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.w900,
-                        ),
-                        hintText: "Title"),
-                  ),
-                ),
-                SizedBox(height: 10),
-                Container(
-                  width: 300,
-                  padding: EdgeInsets.only(
-                      bottom: MediaQuery.of(context).viewInsets.bottom),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: TextFormField(
-                    controller: desController,
-                    decoration: InputDecoration(
-                        contentPadding:
-                            EdgeInsets.symmetric(vertical: 40, horizontal: 10),
-                        filled: true,
-                        fillColor: Colors.grey,
-                        hintStyle: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.w900,
-                        ),
-                        hintText: "Description"),
-                  ),
-                ),
-                SizedBox(height: 10),
-                Container(
-                  width: 300,
-                  padding: EdgeInsets.only(
-                      bottom: MediaQuery.of(context).viewInsets.bottom),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: TextFormField(
-                    controller: dateController,
-                    decoration: InputDecoration(
-                        contentPadding:
-                            EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-                        filled: true,
-                        fillColor: Colors.grey,
-                        hintStyle: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.w900,
-                        ),
-                        hintText: "Date"),
-                  ),
-                ),
-                SizedBox(height: 10),
-                Row(
-                  children: List.generate(
-                    4,
-                    (index) => CircleAvatar(
-                      backgroundColor: Colors.green,
+  Future<dynamic> bottomSheet(BuildContext context) {
+    return showModalBottomSheet(
+      // isScrollControlled: true,
+      context: context,
+      builder: (context) {
+        return Column(
+          // mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            // Expanded(
+            //   child: ListView.separated(
+            //     itemBuilder: (context, index) => Container(
+            //       width: 300,
+            //       decoration: BoxDecoration(
+            //         borderRadius: BorderRadius.circular(20),
+            //       ),
+            //       child: TextField(
+            //         decoration: InputDecoration(
+            //           contentPadding: EdgeInsets.symmetric(
+            //               vertical: 10, horizontal: 20),
+            //           filled: true,
+            //           fillColor: Colors.grey,
+            //           hintStyle: TextStyle(
+            //             fontSize: 18,
+            //             fontWeight: FontWeight.w900,
+            //           ),
+            //           hintText: bottomSheetContents[index],
+            //         ),
+            //       ),
+            //     ),
+            //     separatorBuilder: (context, index) => SizedBox(height: 10),
+            //     itemCount: 3,
+            //   ),
+            // ),
+            SizedBox(height: 10),
+            Container(
+              width: 300,
+              height: 40,
+              padding: EdgeInsets.only(
+                bottom: MediaQuery.of(context).viewInsets.bottom,
+              ),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: TextFormField(
+                controller: nameController,
+                decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.grey,
+                    hintStyle: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w900,
                     ),
+                    hintText: "Title"),
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              width: 300,
+              padding: EdgeInsets.only(
+                  bottom: MediaQuery.of(context).viewInsets.bottom),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: TextFormField(
+                controller: desController,
+                decoration: InputDecoration(
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 40, horizontal: 10),
+                    filled: true,
+                    fillColor: Colors.grey,
+                    hintStyle: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w900,
+                    ),
+                    hintText: "Description"),
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              width: 300,
+              padding: EdgeInsets.only(
+                  bottom: MediaQuery.of(context).viewInsets.bottom),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: TextFormField(
+                controller: dateController,
+                decoration: InputDecoration(
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                    filled: true,
+                    fillColor: Colors.grey,
+                    hintStyle: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w900,
+                    ),
+                    hintText: "Date"),
+              ),
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: List.generate(
+                DataBase.colorList.length,
+                (index) => CircleAvatar(
+                  radius: 30,
+                  backgroundColor: DataBase.colorList[index],
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                addList.add(
+                  NotesModel(
+                    title: nameController.text,
+                    description: desController.text,
+                    date: dateController.text,
                   ),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    addList.add(
-                      NotesModel(
-                        title: nameController.text,
-                        description: desController.text,
-                        date: dateController.text,
-                      ),
-                    );
-                    setState(() {});
-                    nameController.clear();
-                    desController.clear();
-                    dateController.clear();
-                    Navigator.pop(context);
-                  },
-                  child: Text(""),
-                ),
-                SizedBox(height: 10),
-              ],
-            );
-          },
+                );
+                setState(() {});
+                nameController.clear();
+                desController.clear();
+                dateController.clear();
+                Navigator.pop(context);
+              },
+              child: Text(value),
+            ),
+            SizedBox(height: 10),
+          ],
         );
       },
-      child: Icon(
-        Icons.add,
-      ),
     );
   }
 }
